5e81fe6ec2546e0ce5ad47304d3aec4d16c703d7
AriefRuer <arffnajwan30@gmail.com>
2024-06-29 18:55:04 +0800

    Update TBot.h

--- TBot.h
+++ TBot.h
@@ -1,26 +1,80 @@
-#ifndef ROBOT_H
-#define ROBOT_H
+#ifndef BOT_H
+#define BOT_H
 
-#include <vector>
+#include "Grid.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+
 class Robot {
+protected:
+    string name;
+    int numOfLives = 3;
+    int kills = 0;
+    int x, y; // Position on the grid
+    Grid* grid;
+
 public:
-    Robot(int id, int x, int y, bool canLook, bool canMove, bool canFire);
+    Robot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
 
-    void performActions(vector<vector<char>>& battlefield);
+    virtual ~Robot() {}
 
-private:
-    int id;
-    int x, y;
-    bool canLook;
-    bool canMove;
-    bool canFire;
+    int getX() const { return x; }
+    int getY() const { return y; }
+    int getLivesLeft() const { return numOfLives; }
+    int getKills() const { return kills; }
+    void setPosition(int _x, int _y) { x = _x; y = _y; }
+    void PointerToGrid(int* newX, int* newY);
 
-    void look(const vector<vector<char>>& battlefield);
-    void move(vector<vector<char>>& battlefield);
-    void fire(vector<vector<char>>& battlefield);
+    string getName() const { return name; }
+
+};
+class MovingRobot : public virtual Robot
+{
+public:
+    MovingRobot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
+    void Move(Grid* grid, int newX, int newY);
+
+    virtual ~MovingRobot() {} // destructor
 };
 
-#endif // ROBOT_H
+class SeeingRobot : public virtual Robot {
+    public:
+        SeeingRobot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
+        void Look(char **grid, int width, int height);
+
+        virtual ~SeeingRobot() {} // destructor
+
+};
+
+class ShootingRobot : public virtual Robot
+{
+public:
+    ShootingRobot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
+    void Fire(char **grid, int width, int height);
+
+    virtual ~ShootingRobot() {} // destructor
+};
+
+class SteppingRobot : public virtual Robot
+{
+    public:
+    SteppingRobot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
+    void Step(char** grid, int width, int height);
+
+    virtual ~SteppingRobot() {}
+};
+
+
+
+class SimpleBot : public virtual Robot, public SeeingRobot, public ShootingRobot, public SteppingRobot, public MovingRobot { //inheritance
+public:
+    SimpleBot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid* _grid);
+
+    virtual ~SimpleBot() {}
+
+};
+
+#endif // BOT_H
