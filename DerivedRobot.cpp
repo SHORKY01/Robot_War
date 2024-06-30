ddab83fe8a4e2b6496a526df26c377f7694795c9
AriefRuer <arffnajwan30@gmail.com>
2024-06-30 18:20:48 +0800

    Update DerivedRobot.cpp

--- DerivedRobot.cpp
+++ DerivedRobot.cpp
@@ -62,17 +62,155 @@ void RoboCop::Fire(char **grid, int width, int height)
         }
     }
 }
-
+//terminator
 Terminator::Terminator(string _name, int _numOfLives, int _kills, int _x, int _y, Grid *_grid)
-    : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid)
-{
-}
+    : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid) {}
 
+void Terminator::Look(char **grid, int width, int height){
+    cout << '\n'
+         << name << " is looking around " << endl;
+    bool Enemy = false;
+    for (int W = -1; W <= 1; ++W)
+    {
+        for (int H = -1; H <= 1; ++H)
+        {
+            int lookX = getX() + W;
+            int lookY = getY() + H;
+
+            // Check bounds to avoid out-of-bounds access
+            if (lookX >= 0 && lookX < width && lookY >= 0 && lookY < height)
+            {
+                if (grid[lookY][lookX] != '.' && !(lookX == x && lookY == y)) // to make sure it doesnt detect itself as enemy
+                {
+                    Enemy = true;
+                    cout << "->" << name << " found enemy robot at " << "(" << lookX << "," << lookY << "): " << grid[lookY][lookX] << endl;
+                    break;
+                }
+                if (Enemy == true)
+                    break;
+            }
+        }
+    }
+    if (Enemy == false)
+    {
+        cout << "->" << name << " sees nothing in a 9 square area." << endl;
+    }
+    this->grid->display();
+}
+void Terminator::Move(Grid *grid, int newX, int newY)
+{
+    // Scan for enemy robots
+    pair<int, int> enemyPositions[8];
+    int enemyCount = 0;
+    int width = grid->getWidth();
+    int height = grid->getHeight();
+
+    for (int W = -1; W <= 1; ++W)
+    {
+        for (int H = -1; H <= 1; ++H)
+        {
+            int lookX = getX() + W;
+            int lookY = getY() + H;
+
+            // Check bounds to avoid out-of-bounds access
+            if ((lookX >= 0 && lookX < width) && (lookY >= 0 && lookY < height))
+            {
+                if ((grid->getGrid()[lookY][lookX] != '.') && !(lookX == getX() && lookY == getY()))
+                { // to make sure it doesn't detect itself as enemy
+                    if (enemyCount < 8)
+                    {
+                        enemyPositions[enemyCount++] = {lookX, lookY};
+                    }
+                }
+            }
+        }
+    }
+
+    // Move to an enemy position if found
+    if (enemyCount > 0)
+    {
+        int targetIndex = rand() % enemyCount;
+        newX = enemyPositions[targetIndex].first;
+        newY = enemyPositions[targetIndex].second;
+    }
+    else
+    {
+        // If no enemies are found, move randomly
+        srand(time(0));
+        int choice_dir = rand() % 8 + 1;
+        newX = getX();
+        newY = getY();
+        switch (choice_dir)
+        {
+        case 1:
+            newY -= 1;
+            break;
+        case 2:
+            newY += 1;
+            break;
+        case 3:
+            newX += 1;
+            break;
+        case 4:
+            newX -= 1;
+            break;
+        case 5:
+            newX -= 1;
+            newY -= 1;
+            break;
+        case 6:
+            newX += 1;
+            newY -= 1;
+            break;
+        case 7:
+            newX -= 1;
+            newY += 1;
+            break;
+        case 8:
+            newX += 1;
+            newY += 1;
+            break;
+        }
+    }
+
+    // Update position
+    if (newX >= 0 && newX < grid->getWidth() && newY >= 0 && newY < grid->getHeight())
+    {
+        if (!(newX == getX() && newY == getY()))
+        {
+            grid->clearPosition(x, y);
+            if (grid->getGrid()[newY][newX] == '.')
+            {
+                cout << "\n->" << getName() << " moved to (" << newX << ", " << newY << ")" << endl;
+                x = newX;
+                y = newY;
+                grid->placeBot(newX, newY, this->getName()[0]);
+            }
+            else
+            {
+                cout << "\n->" << "Robot is stepped on!: " << grid->getGrid()[newY][newX] << endl;
+                kills = getKills() + 1;
+                cout << "\n->" << getName() << " now has " << getKills() << " kills!\n";
+                x = newX;
+                y = newY;
+                grid->placeBot(newX, newY, this->getName()[0]);
+            }
+        }
+    }
+    else
+    {
+        cout << "->" << "Invalid point. Position out of bounds." << endl;
+    }
+    this->grid->display();
+}
+//----------------------------------------------------------------------------------------------------------------
 TerminatorRoboCop::TerminatorRoboCop(string _name, int _numOfLives, int _kills, int _x, int _y, Grid *_grid)
     : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid) {}
 
 BlueThunder::BlueThunder(string _name, int _numOfLives, int _kills, int _x, int _y, Grid *_grid)
-    : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid) {}
+    : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid)
+{
+}
 
 Madbot::Madbot(string _name, int _numOfLives, int _kills, int _x, int _y, Grid *_grid)
     : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid) {}
