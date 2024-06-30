725790c8fa90938f30fa48d33447b23dcc7845c2
AriefRuer <arffnajwan30@gmail.com>
2024-06-30 13:52:52 +0800

    Update DerivedRobot.cpp

--- DerivedRobot.cpp
+++ DerivedRobot.cpp
@@ -5,6 +5,7 @@
 #include "TBot.h"
 #include "DerivedRobot.h"
 
+
 RoboCop::RoboCop(string _name, int _numOfLives, int _kills, int _x, int _y, Grid *_grid)
     : Robot(_name, 3, _kills, _x, _y, _grid), SeeingRobot(_name, 3, _kills, _x, _y, _grid), ShootingRobot(_name, 3, _kills, _x, _y, _grid), MovingRobot(_name, 3, _kills, _x, _y, _grid) {}
 
@@ -21,7 +22,6 @@ void RoboCop::Fire(char **grid, int width, int height)
     int missedShots[3][2]; // To store up to 3 missed shot coordinates
     int missedCount = 0;
 
-    bool missed = false;
     for (int i = 0; i < 3; i++){ //fire 3 times
          // Generate random offsets between -5 and 5
         int RandX = rand() % 11 - 5;
@@ -30,22 +30,27 @@ void RoboCop::Fire(char **grid, int width, int height)
         int newX = getX() + RandX;
         int newY = getY() + RandY;
 
-        if (newX >= 0 && newX < width && newY >= 0 && newY < height){
+        if ((newX >= 0 && newX < width) && (newY >= 0 && newY < height) && !(newX == getX() && newY == getY()))
+        {
             //shoot at the new coordinates
-            grid[newY][newX] = '*'; //coordinates hit symbol
-            if (grid[newY][newX] != '.' && grid[newY][newX] != '*'){
-                cout << "\n->" << this->name << " fires at (" << newX << ", " << newY << "): " << grid[newY][newX] << endl;
+            char hitrobot = grid[newY][newX] = '*'; //coordinates hit symbol
+            if ((hitrobot != '.') && (hitrobot != '*')){
+                cout << "\n->" << this->name << " fires at (" << newX << ", " << newY << "): " << hitrobot << endl;
                 kills = getKills() + 1;
             }
             else {
+                cout << "\n->" << this->name << " misses at (" << newX << ", " << newY << ")" << endl;
                 grid[newY][newX] = '*'; // Mark missed shots directly on the grid
                 missedShots[missedCount][0] = newX;
                 missedShots[missedCount][1] = newY;
                 missedCount++;
             }
         }
+        else {
+            cout << "\n->" << "Position shot out of bounds!" << endl;
+        }
     }
-    if (missed) {
+    if (missedCount > 0) {
         cout << endl;
         this->grid->display(); // call the display fx on the grid member variable
         // Restore the grid by changing '*' back to '.' because '*' is just a hitmarket that shows where spots are hit on the grid
